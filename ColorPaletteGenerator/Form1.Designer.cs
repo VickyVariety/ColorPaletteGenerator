8441285a73a6e95c6c6f3030fbdab2a67d3c1cf3
VickyVariety <vickyfyrehart@gmail.com>
2024-11-06 00:32:39 +0100

    Initial commit

---
+++ ColorPaletteGenerator/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace ColorPaletteGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBoxHex = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(450, 472);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 66);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate Palette";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(206, 281);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(124, 126);
+            this.panel1.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Location = new System.Drawing.Point(346, 281);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(124, 126);
+            this.panel2.TabIndex = 7;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Location = new System.Drawing.Point(485, 281);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(124, 126);
+            this.panel3.TabIndex = 9;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Location = new System.Drawing.Point(625, 281);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(124, 126);
+            this.panel4.TabIndex = 11;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Location = new System.Drawing.Point(767, 281);
+            this.panel5.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(124, 126);
+            this.panel5.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AccessibleDescription = "Kopiera till urklipp";
+            this.label1.AccessibleName = "";
+            this.label1.AutoSize = true;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label1.Location = new System.Drawing.Point(220, 410);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Color 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AccessibleDescription = "Kopiera till urklipp";
+            this.label2.AccessibleName = "";
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Location = new System.Drawing.Point(359, 410);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Color 2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AccessibleDescription = "Kopiera till urklipp";
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Location = new System.Drawing.Point(498, 410);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Color 3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AccessibleDescription = "Kopiera till urklipp";
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Location = new System.Drawing.Point(638, 410);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Color 4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AccessibleDescription = "Kopiera till urklipp";
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.Location = new System.Drawing.Point(780, 410);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Color 5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // checkBoxHex
+            // 
+            this.checkBoxHex.AutoSize = true;
+            this.checkBoxHex.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.checkBoxHex.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.checkBoxHex.FlatAppearance.BorderSize = 2;
+            this.checkBoxHex.FlatAppearance.CheckedBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.checkBoxHex.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxHex.Location = new System.Drawing.Point(662, 495);
+            this.checkBoxHex.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.checkBoxHex.Name = "checkBoxHex";
+            this.checkBoxHex.Size = new System.Drawing.Size(99, 23);
+            this.checkBoxHex.TabIndex = 19;
+            this.checkBoxHex.Text = "Visa HEX";
+            this.checkBoxHex.UseVisualStyleBackColor = true;
+            this.checkBoxHex.CheckedChanged += new System.EventHandler(this.checkBoxHex_CheckedChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Black", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(262, 123);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(584, 60);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Color Palette Generator";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Enabled = false;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(925, 324);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 39);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Export Palette";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(1165, 685);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBoxHex);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Color Palette Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBoxHex;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ ColorPaletteGenerator/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ColorPaletteGenerator
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random(); // Random instance used for generating colors.
+
+        // Variables to store both RGB and HEX
+        private string[] rgbValues = new string[5];
+        private string[] hexValues = new string[5];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Method to generate a random color by producing random RGB values.
+        private Color GenerateRandomColor()
+        {
+            int r = random.Next(0, 256); // Red value (0-255)
+            int g = random.Next(0, 256); // Green value (0-255)
+            int b = random.Next(0, 256); // Blue value (0-255)
+            return Color.FromArgb(r, g, b); // Create a color from RGB components.
+        }
+
+        // Method to convert a Color object to a HEX string representation.
+        private string ColorToHex(Color color)
+        {
+            return $"{color.R:X2}{color.G:X2}{color.B:X2}"; // Format RGB values to HEX.
+        }
+
+        // Method to generate a new palette and store both RGB and HEX values.
+        private void UpdatePalette()
+        {
+            // Generate random colors for each panel.
+            Color color1 = GenerateRandomColor();
+            Color color2 = GenerateRandomColor();
+            Color color3 = GenerateRandomColor();
+            Color color4 = GenerateRandomColor();
+            Color color5 = GenerateRandomColor();
+
+            // Update the color of each panel.
+            panel1.BackColor = color1;
+            panel2.BackColor = color2;
+            panel3.BackColor = color3;
+            panel4.BackColor = color4;
+            panel5.BackColor = color5;
+
+            // Store RGB and HEX values in arrays
+            rgbValues[0] = $"RGB: {color1.R}, {color1.G}, {color1.B}";
+            rgbValues[1] = $"RGB: {color2.R}, {color2.G}, {color2.B}";
+            rgbValues[2] = $"RGB: {color3.R}, {color3.G}, {color3.B}";
+            rgbValues[3] = $"RGB: {color4.R}, {color4.G}, {color4.B}";
+            rgbValues[4] = $"RGB: {color5.R}, {color5.G}, {color5.B}";
+
+            hexValues[0] = $"HEX: {ColorToHex(color1)}";
+            hexValues[1] = $"HEX: {ColorToHex(color2)}";
+            hexValues[2] = $"HEX: {ColorToHex(color3)}";
+            hexValues[3] = $"HEX: {ColorToHex(color4)}";
+            hexValues[4] = $"HEX: {ColorToHex(color5)}";
+
+            // Show labels according to the checkbox state (RGB or HEX)
+            UpdateLabels();
+            button2.Enabled = true;  // Enable Export-button after a palette has been generated
+        }
+
+        // Method to update the labels based on the checkbox state
+        private void UpdateLabels()
+        {
+            // If the checkbox is checked, display HEX values; otherwise, display RGB values
+            if (checkBoxHex.Checked)
+            {
+                ShowHEX(); // Show HEX when checkbox is checked
+            }
+            else
+            {
+                ShowRGB(); // Show RGB when checkbox is unchecked
+            }
+        }
+
+        // Method to show RGB values
+        private void ShowRGB()
+        {
+            label1.Text = rgbValues[0];
+            label2.Text = rgbValues[1];
+            label3.Text = rgbValues[2];
+            label4.Text = rgbValues[3];
+            label5.Text = rgbValues[4];
+        }
+
+        // Method to show HEX values
+        private void ShowHEX()
+        {
+            label1.Text = hexValues[0];
+            label2.Text = hexValues[1];
+            label3.Text = hexValues[2];
+            label4.Text = hexValues[3];
+            label5.Text = hexValues[4];
+        }
+
+        // Event handler for the button click to generate a new color palette.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdatePalette(); // Generate a new palette
+        }
+
+        // Event handler for checkbox state change to toggle between RGB and HEX
+        private void checkBoxHex_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateLabels(); // Update labels to reflect the current state of the checkbox
+        }
+
+        // Event handler for label1 click to copy the color code to the clipboard.
+        private void label1_Click(object sender, EventArgs e)
+        {
+            string labelText = label1.Text;
+
+            // Copy the color code to clipboard.
+            if (labelText.StartsWith("RGB"))
+            {
+                Clipboard.SetText(labelText.Substring(5).Trim());
+            }
+
+            else if (labelText.StartsWith("HEX"))
+            {
+                Clipboard.SetText(labelText.Substring(5).Trim());
+            }
+
+            MessageBox.Show("Color code copied to clipboard!"); // Show confirmation message.
+        }
+
+        // Event handler for label2 click to copy the color code to the clipboard.
+        private void label2_Click(object sender, EventArgs e)
+        {
+            string labelText = label2.Text;
+
+            // Copy the color code to clipboard.
+            if (labelText.StartsWith("RGB"))
+            {
+                Clipboard.SetText(labelText.Substring(5).Trim());
+            }
+
+            else if (labelText.StartsWith("HEX"))
+            {
+                Clipboard.SetText(labelText.Substring(5).Trim());
+            }
+
+            MessageBox.Show("Color code copied to clipboard!"); // Show confirmation message.
+        }
+
+        // Event handler for label3 click to copy the color code to the clipboard.
+        private void label3_Click(object sender, EventArgs e)
+        {
+            string labelText = label3.Text;
+
+            // Copy the color code to clipboard.
+            if (labelText.StartsWith("RGB"))
+            {
+                Clipboard.SetText(labelText.Substring(5).Trim());
+            }
+
+            else if (labelText.StartsWith("HEX"))
+            {
+                Clipboard.SetText(labelText.Substring(5).Trim());
+            }
+
+            MessageBox.Show("Color code copied to clipboard!"); // Show confirmation message.
+        }
+
+        // Event handler for label4 click to copy the color code to the clipboard.
+        private void label4_Click(object sender, EventArgs e)
+        {
+            string labelText = label4.Text;
+
+            // Copy the color code to clipboard.
+            if (labelText.StartsWith("RGB"))
+            {
+                Clipboard.SetText(labelText.Substring(5).Trim());
+            }
+
+            else if (labelText.StartsWith("HEX"))
+            {
+                Clipboard.SetText(labelText.Substring(5).Trim());
+            }
+
+            MessageBox.Show("Color code copied to clipboard!"); // Show confirmation message.
+        }
+
+        // Event handler for label5 click to copy the color code to the clipboard.
+        private void label5_Click(object sender, EventArgs e)
+        {
+            string labelText = label5.Text;
+
+            // Copy the color code to clipboard.
+            if (labelText.StartsWith("RGB"))
+            {
+                Clipboard.SetText(labelText.Substring(5).Trim());
+            }
+
+            else if (labelText.StartsWith("HEX"))
+            {
+                Clipboard.SetText(labelText.Substring(5).Trim());
+            }
+
+            MessageBox.Show("Color code copied to clipboard!"); // Show confirmation message.
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Create bitmap 400px width, 100px height
+            Bitmap bitmap = new Bitmap(400, 100);
+
+            // Draw color boxes on the bitmap (use panels colors)
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                // Panels colors, each and one is width 80px (400 / 5 = 80)
+                g.FillRectangle(new SolidBrush(panel1.BackColor), new Rectangle(0, 0, 80, 100));
+                g.FillRectangle(new SolidBrush(panel2.BackColor), new Rectangle(80, 0, 80, 100));
+                g.FillRectangle(new SolidBrush(panel3.BackColor), new Rectangle(160, 0, 80, 100));
+                g.FillRectangle(new SolidBrush(panel4.BackColor), new Rectangle(240, 0, 80, 100));
+                g.FillRectangle(new SolidBrush(panel5.BackColor), new Rectangle(320, 0, 80, 100));
+            }
+
+            // Use SaveFileDialog to let the user choose where to save the image
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                {
+                    saveFileDialog.Filter = "PNG Image|*.png";
+                    saveFileDialog.Title = "Save Palette as PNG";
+                    saveFileDialog.FileName = "palette.png";
+
+                    if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                    {
+                        // Save bitmap as PNG
+                        bitmap.Save(saveFileDialog.FileName, System.Drawing.Imaging.ImageFormat.Png);
+                        MessageBox.Show("Palette saved!", "Export Palette", MessageBoxButtons.OK, MessageBoxIcon.Information); // Show confirmation message.
+                    }
+                }
+            }
+
+        }
+    }
+}
